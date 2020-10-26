1fe2da1f9dbe9b2a018bd44b65baad5dc46b69ac
Tai Groot <tai@taigrr.com>
2020-10-26 05:09:21 -0700

    added ddg

--- background.js
+++ background.js
@@ -4,8 +4,9 @@ function announce_close(){
 		var closeable=[];
 		for (var i=0; i<tabs.length; ++i) {
 			console.log(tabs[i]);
-			if(tabs[i].url.includes("https://www.google.com/search?"))
-			{	
+			if(tabs[i].url.includes("google.com/search?")) {	
+				closeable.push(tabs[i].id)
+			} else if(tabs[i].url.includes("duckduckgo.com/?q=")){
 				closeable.push(tabs[i].id)
 			}
 			chrome.tabs.remove(closeable);
