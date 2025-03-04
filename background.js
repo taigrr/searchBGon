dea86d51fdf2f6eb16ad7c4de10416aec6147dc0
Tai Groot <tai@taigrr.com>
2025-03-04 00:05:37 -0800

    arrow func

--- background.js
+++ background.js
@@ -1,5 +1,5 @@
 chrome.action.onClicked.addListener(() => {
-	chrome.tabs.query({}, function(tabs) {
+	chrome.tabs.query({}, (tabs) =>{
 		chrome.tabs.remove(tabs.filter(tab => {
 			return tab.url.includes("google.com/search?") || tab.url.includes("duckduckgo.com/?")
 		}).map(tab => tab.id));
